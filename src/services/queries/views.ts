c71145d62bfe8ec45dad57d35486bd74a50eb393
shreyan naskar <shreyannaskar@gmail.com>
2026-07-18 21:33:34 +0530

    lua scripting

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,14 +1,5 @@
-import { itemsByViewsKey, itemsKey, itemsViewsKey } from "$services/keys";
 import { client } from "$services/redis";
 
 export const incrementView = async (itemId: string, userId: string) => {
-    // use the hyperloglog structure to insert
-    const inserted = await client.pfAdd(itemsViewsKey(itemId), userId)
-    // if new insert
-    if(inserted){
-        return Promise.all([
-            client.hIncrBy(itemsKey(itemId), 'views', 1),
-            client.zIncrBy(itemsByViewsKey(), 1, itemId)
-        ])
-    }
+    return await client.incrementView(itemId, userId)
 };
--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,11 +1,53 @@
-import { createClient } from 'redis';
+import { itemsByViewsKey, itemsKey, itemsViewsKey } from '$services/keys';
+import { createClient, defineScript } from 'redis';
 
 const client = createClient({
 	socket: {
 		host: process.env.REDIS_HOST,
 		port: parseInt(process.env.REDIS_PORT)
 	},
-	password: process.env.REDIS_PW
+	password: process.env.REDIS_PW,
+	// custom function to execute LUA scripts
+	scripts: {
+		addOneAndStore: defineScript({
+			NUMBER_OF_KEYS: 1,
+			SCRIPT: `
+				return redis.call('SET', KEYS[1], 1 + tonumber(ARGV[1]))
+			`,
+			transformArguments(value: number, key: string){
+				return [key, value.toString()]
+			},
+			transformReply(reply: any) {
+				return reply
+			},
+		}),
+		incrementView: defineScript({
+			NUMBER_OF_KEYS: 3,
+			SCRIPT: `
+				local itemsViewsKey = KEYS[1]
+				local itemsKey = KEYS[2]
+				local itemsByViewsKey = KEYS[3]
+				local itemId = ARGV[1]
+				local userId = ARGV[2]
+
+				local inserted = redis.call('PFADD', itemsViewsKey, userId)
+				if inserted == 1 then
+					redis.call('HINCRBY', itemsKey, 'views', 1)
+					redis.call('ZINCRBY', itemsByViewsKey, 1, itemId)
+				end
+			`,
+			transformArguments(itemId: string, userId: string){
+				return [
+					itemsViewsKey(itemId),
+					itemsKey(itemId),
+					itemsByViewsKey(),
+					itemId,
+					userId
+				]
+			},
+			transformReply() {}
+		})
+	}
 });
 
 client.on('error', (err) => console.error(err));
