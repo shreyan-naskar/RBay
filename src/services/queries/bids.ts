8e443f7a9ffac485a6ebf408a7406c5f5316c9e8
shreyan naskar <shreyannaskar@gmail.com>
2026-07-19 12:04:37 +0530

    locks for consistency

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,18 +1,13 @@
 import { bidHistoryKey, itemsByPriceKey, itemsKey } from '$services/keys';
-import { client } from '$services/redis';
+import { client, pause, withLock } from '$services/redis';
 import type { CreateBidAttrs, Bid } from '$services/types';
 import { DateTime } from 'luxon';
 import { getItem } from './items';
 import { attr } from 'svelte/internal';
 
 export const createBid = async (attrs: CreateBidAttrs) => {
-	return client.executeIsolated(async (isolatedClient)=> {
-		// watch for concurrency
-		await isolatedClient.watch(itemsKey(attrs.itemId))
-
-
+	return withLock(attrs.itemId, async (lockedClient: typeof client, signal: any) =>{
 		const item = await getItem(attrs.itemId)
-
 		// validation checks
 		// only allow bid on existing item
 		if(!item){
@@ -30,21 +25,64 @@ export const createBid = async (attrs: CreateBidAttrs) => {
 			attrs.amount,
 			attrs.createdAt.toMillis()
 		)
+
+		if (signal.expired){
+			throw new Error(`Lock Expired can't write`)
+		}
 		// add bids to the list
-		return isolatedClient
-			.multi()
-			.rPush(bidHistoryKey(attrs.itemId), serialized)
-			.hSet(itemsKey(item.id), {
+		return await Promise.all([
+			lockedClient.rPush(bidHistoryKey(attrs.itemId), serialized),
+			lockedClient.hSet(itemsKey(item.id), {
 				bids: item.bids + 1,
 				price: attrs.amount,
 				highestBidUserId: attrs.userId
-			})
-			.zAdd(itemsByPriceKey(), {
+			}),
+			lockedClient.zAdd(itemsByPriceKey(), {
 				value: item.id,
 				score: attrs.amount
 			})
-			.exec()
+		])
 	})
+
+	// return client.executeIsolated(async (isolatedClient)=> {
+	// 	// watch for concurrency
+	// 	await isolatedClient.watch(itemsKey(attrs.itemId))
+
+
+	// 	const item = await getItem(attrs.itemId)
+
+	// 	// validation checks
+	// 	// only allow bid on existing item
+	// 	if(!item){
+	// 		throw new Error("Item does not exist!")
+	// 	}
+	// 	if(item.endingAt.diff(DateTime.now()).toMillis() < 0){
+	// 		throw new Error("Item closed to bidding!")
+	// 	}
+	// 	// higher bid is valid
+	// 	if(item.price >= attrs.amount){
+	// 		throw new Error("Bid too low!")
+	// 	}
+
+	// 	const serialized = serializeHistory(
+	// 		attrs.amount,
+	// 		attrs.createdAt.toMillis()
+	// 	)
+	// 	// add bids to the list
+	// 	return isolatedClient
+	// 		.multi()
+	// 		.rPush(bidHistoryKey(attrs.itemId), serialized)
+	// 		.hSet(itemsKey(item.id), {
+	// 			bids: item.bids + 1,
+	// 			price: attrs.amount,
+	// 			highestBidUserId: attrs.userId
+	// 		})
+	// 		.zAdd(itemsByPriceKey(), {
+	// 			value: item.id,
+	// 			score: attrs.amount
+	// 		})
+	// 		.exec()
+	// })
 };
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
--- src/services/redis/lock.ts
+++ src/services/redis/lock.ts
@@ -1,8 +1,72 @@
-export const withLock = async () => {};
+import { randomBytes } from "crypto";
+import { client } from "./client";
+import { ClientClosedError } from "redis";
 
-const buildClientProxy = () => {};
+export const withLock = async (key: string, cb: (redisClient: Client, signal: any) => any) => {
+	// init vars to control retries
+	const retryDelayMs = 100;
+	const timeoutMs = 2000
+	let retries = 20
 
-const pause = (duration: number) => {
+	// random value to store at lock key
+	const token = randomBytes(6).toString('hex')
+	
+	// create a lock key
+	const lockKey = `lock:${key}`
+	
+	// retry logic
+	while(retries >= 0){
+		retries--
+
+		// try to acquire lock
+		const acquiredLock = await client.set(lockKey, token, {
+			NX: true,
+			PX: timeoutMs
+		})
+
+		// if loack busy
+		if (!acquiredLock){
+			// pause before retry
+			await pause(retryDelayMs)
+			continue
+		}
+
+		// if lock acquired
+		try{
+			const signal = {
+				expired: false
+			}
+			setTimeout(() => {
+				signal.expired = true
+			}, timeoutMs)
+			const proxiedClient = buildClientProxy(timeoutMs)
+			const result = await cb(proxiedClient, signal)
+			return result
+		}finally{
+			// unlock iff lock value is same
+			await client.unlock(lockKey, token)
+		}
+	}
+};
+
+type Client = typeof client
+const buildClientProxy = (timeoutMs: number) => {
+	const startTime = Date.now()
+
+	const handler = {
+		get(target: Client, prop: keyof Client) {
+			if(Date.now() >= startTime + timeoutMs){
+				throw new Error('Lock has expired')
+			}
+		const value = target[prop]
+		return typeof value === 'function' ? value.bind(target) : value
+		}
+	}
+
+	return new Proxy(client, handler) as Client
+};
+
+export const pause = (duration: number) => {
 	return new Promise((resolve) => {
 		setTimeout(resolve, duration);
 	});
